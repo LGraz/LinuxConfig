56ae276552da4edb8328d574850b60e65ca188d0
LukasGraz <lukasgraz99@gmail.com>
2022-10-25 16:20:47 +0200

    code-OSS --> vscode

---
+++ installation-scripts/10-code.sh
@@ -0,0 +1,9 @@
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-python.python
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension reditorsupport.r
+code --install-extension rdebugger.r-debugger
+code --install-extension eamodio.gitlens
+code --install-extension christian-kohler.path-intellisense
+code --install-extension james-yu.latex-workshop
