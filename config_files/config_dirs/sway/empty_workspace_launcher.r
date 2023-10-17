1265e47b7cacccbea41ddac4dad7c7048447c9da
LukasGraz <lukasgraz99@gmail.com>
2023-10-17 16:38:21 +0200

    .

--- config_files/config_dirs/sway/empty_workspace_launcher.r
+++ config_files/config_dirs/sway/empty_workspace_launcher.r
@@ -9,9 +9,9 @@ workspaces <- system("swaymsg -p -t get_workspaces", intern = TRUE)
 # Sys.sleep(2)
 focused <- grep(" \\(focused\\)", workspaces)
 is_empty_workspace <- grepl("Representation: ([a-zA-Z]\\[\\]|\\(null\\))", workspaces[focused + 3])
-print(workspaces)
+
 cat("Workspace empty:", is_empty_workspace, "\n")
 if (is_empty_workspace) {
-  print(paste("Execute:", args[2], "&"))
-  system(paste(args[2], "&"))
+  print(paste("Execute:", paste0("swaymsg \'exec ", args[2], "\'")))
+  system(paste0("swaymsg \'exec ", args[2], "\'"))
 }
