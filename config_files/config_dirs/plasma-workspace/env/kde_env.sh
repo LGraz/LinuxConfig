d12f1f52dc91c9e9dd443ac0fc8912ff68866c5e
LukasGraz <lukasgraz99@gmail.com>
2025-05-07 11:08:44 +0200

    .

---
+++ config_files/config_dirs/plasma-workspace/env/kde_env.sh
@@ -0,0 +1,7 @@
+export PATH="$HOME/LinuxConfig/bin:$HOME/.local/bin:$HOME/.cargo/bin:$HOME/.npm-global/bin:$PATH:/usr/bin/vendor_perl"
+export BROWSER=/bin/firefox
+export TERMINAL=/usr/bin/kitty
+export EDITOR=/usr/bin/micro
+export VISUAL=/usr/bin/code
+export KDE_ENV=TRUE
+export SHELL=/bin/zsh
